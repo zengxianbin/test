a300492b8eac46e0dfdd35f55ef2bb1e79c7c9e5
zengxianbin91@163.com <zengxianbin91@163.com>
2020-01-06 21:41:56 +0800

    a java web sample

---
+++ java2/src/main/java/com/ghz/testweb/App.java
@@ -0,0 +1,13 @@
+package com.ghz.testweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! i am oldboy22333" );
+    }
+}
